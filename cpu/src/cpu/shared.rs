90dc3f58522f38aaace491673bfc06127e1e74ca
owl-from-hogvarts <transftank@mail.ru>
2024-05-07 19:47:21 +0300

    fix: correct `status` register display format

--- cpu/src/cpu/shared.rs
+++ cpu/src/cpu/shared.rs
@@ -8,13 +8,24 @@ pub struct Status {
 
 impl Display for Status {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut flags: Vec<String> = Vec::new();
+
         if self.zero {
-            write!(f, "ZERO")?;
+            flags.push("ZERO".to_owned());
         }
 
         if self.carry {
-            write!(f, ", CARRY")?;
+            flags.push("CARRY".to_owned());
         }
+
+        write!(
+            f,
+            "{}",
+            flags
+                .into_iter()
+                .reduce(|acc, value| acc + ", " + &value)
+                .unwrap_or_default()
+        )?;
 
         Ok(())
     }
